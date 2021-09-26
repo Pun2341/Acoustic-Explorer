f76e52383c869c1b1119a106278d59c6a6466752
thomasporter522 <thomasporter2019@gmail.com>
2021-09-25 21:36:53 -0400

    cleaned up

--- audio.py
+++ audio.py
@@ -1,7 +1,6 @@
 import numpy as np
 import math
 import pygame, pygame.sndarray
-import matplotlib.pyplot as plt
 
 import calculations
 
@@ -10,12 +9,9 @@ pygame.mixer.init(channels=1)
 sample_rate = 44100
 volume = 4096
 duration = 1
-double_mixer = False
+double_mixer = True
 
 def sine_wave(hz, peak, n_samples=sample_rate):
-    """Compute N samples of a sine wave with given frequency and peak amplitude.
-       Defaults to one second.
-    """
     length = sample_rate / float(hz)
     omega = np.pi * 2 / length
     xvalues = np.arange(int(length)) * omega
@@ -27,21 +23,12 @@ def calculate_waveform(intensities, envelope, note):
     waveform = sine_wave(pitch, volume * intensities[0])
     for i in range(1,len(intensities)):
         waveform = np.add(waveform, sine_wave(pitch * (i+1), volume * intensities[i]))
-    #waveform = (waveform*volume)//max(abs(waveform))
     waveform = np.array([int(envelope(i/len(waveform)) * waveform[i]) for i in range(len(waveform))])
     return waveform
-
-#def play_for(sample_wave, ms):
-#    """Play the given NumPy array, as a sound, for ms milliseconds."""
-#    sound = pygame.sndarray.make_sound(np.array([(a,a) for a in sample_wave]))
-#    sound.play(-1)
-#    pygame.time.delay(ms)
-#    sound.stop()
-
 def calculate_waveform_from_vec(v, note):
     intensities, envelope = calculations.calculate_intensities(v)
     waveform = calculate_waveform(intensities, envelope, note)
-    #if double_mixer: waveform = np.array([(a,a) for a in waveform])
+    if double_mixer: waveform = np.array([(a,a) for a in waveform])
     #else:
     waveform = np.array([np.int32(a) for a in waveform])
     return waveform
@@ -49,30 +36,3 @@ def calculate_waveform_from_vec(v, note):
 def calculate_sound(waveform):
     sound = pygame.sndarray.make_sound(waveform)
     return sound
-    
-    	
-if __name__ == "__main__":
-    i,e = list(calculations.calculate_intensities([0.4,1,1,0]))
-    print(i)
-    #i = [1, 0,0.5,0,0.05,0,0.2,0.4,0.43,0.48,0,0.1,0.05] # clarinet
-    #print(i)
-    #i = [1, 0.5, 0.2, 0.21, 0.07] #0.41] # 0.07, 0.3, 0.1, 0.1, 0.03, 0.04, 0.04] #piano?
-    #i = [1, 0.4, 0.15, 0.1, 0.1, 0.05] # violin?
-    #i = [1/j for j in range(1,6)]
-    # a = 0.001
-    # def e(x): 
-    #     if x < a: return x/a
-    #     return (1-x)/(1-a)
-    w = calculate_waveform(i, e, 0)
-    print(w)
-    #sound = pygame.sndarray.make_sound(np.array([(a,a) for a in w]))
-    asdfasdf = np.array([np.int32(a) for a in w])
-    print(type(asdfasdf[100]))
-    print('a')
-    sound = pygame.sndarray.make_sound(asdfasdf)
-    print('b')
-    sound.play(-1)
-    _, axs = plt.subplots(2)
-    axs[0].plot(w)
-    axs[1].bar(range(len(i)), i)
-    plt.show()
--- calculations.py
+++ calculations.py
@@ -6,21 +6,12 @@ num_overtones = 8
 num_neighbors = 8 # please don't change this
 
 def calculate_neighbors(center, radius):
-    # re = [center]
-    # if radius > threshold:
-    #     for i in range(4):
-    #         re += [[min(center[j] + (radius if i == j else 0), 1) for j in range(4)], \
-    #             [max(center[j] - (radius if i == j else 0), 0) for j in range(4)]]
-    # return re
-
     re = []
     new_center = center.copy()
     if radius > threshold: 
         for i in range(len(new_center)):
             if center[i] + radius - 1 > 0: new_center[i] -= (center[i] + radius - 1) 
             elif center[i] - radius < 0: new_center[i] -= (center[i] - radius)
-        #for _ in range(num_neighbors):
-            #re.append([c+random.random() * radius for c in new_center])
         corners = [[-1,-1,-1,-1], [1,1,1,1], [1,1,-1,-1], [1,-1,1,-1], [1,-1,-1,1], [-1,1,1,-1], [-1,1,-1,1], [-1,-1,1,1]]
         for c in corners:
             re.append([new_center[i] + (radius * c[i]) for i in range(4)])
@@ -28,24 +19,6 @@ def calculate_neighbors(center, radius):
     
 
 def calculate_intensities(v):
-    #v = [e*2 - 1 for e in v]
-    #re = []
-    #for n in range(1, num_overtones):
-        # x = 1 - 2*math.exp(-n/2)
-        # y = v[0]
-        # y += v[1] * (x+1)/2
-        # y += v[2] * (0.5*(3*x*x + 1) + 1)/2
-        # y += v[3] * (0.5*(5*x*x*x - 3*x) + 1)/2
-        # re.append(y)
-    #return re
-
-    # basis = np.array([ \
-    #     [1/(n+1) for n in range(num_overtones)], \
-    #     [1/(n+1) if n % 2 == 0 else 0 for n in range(num_overtones)], \
-    #     [1/(n+1) if n % 3 == 0 else 0 for n in range(num_overtones)], \
-    #     [1/(n+1) if n % 4 == 0 else 0 for n in range(num_overtones)]])
-    # return np.matmul(v, basis)
-
     depth_vector = [1/(i+1) for i in range(num_overtones)]
     even_depth_vector = [1/(i+1) if i % 2 == 0 else 0 for i in range(num_overtones)]
     reedy_vector = [0.3 if i == 6 or i == 9 else (0.4 if i == 7 or i == 8 else 0) for i in range(num_overtones)]
@@ -56,15 +29,3 @@ def calculate_intensities(v):
     a = 0.001 + 0.8*v[0]
     envelope = lambda x: (x/a if x < a else (1-x)/(1-a))
     return intensities, envelope
-
-if __name__ == "__main__":
-    center = [0]*4
-    radius = 0.5
-    centers = calculate_neighbors(center, radius)
-    while(len(centers) > 1):
-        for i in range(len(centers)):
-            print(str(i)+": "+str(centers[i]))
-        i = int(input(": "))
-        radius = radius / 2
-        centers = calculate_neighbors(centers[i], radius)
-    print(centers)
